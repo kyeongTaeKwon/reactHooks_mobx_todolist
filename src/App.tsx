921d4db22b6ac42a959d471e6837abb5baf3b2bc
kwon <sjbby100@naver.com>
2020-08-06 22:42:46 +0900

    add new Todo feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { TodoList } from "./features/TodoList";
+import { TodoNew } from "./features/TodoNew";
 function App() {
   return (
     <div className="App">
+      <TodoNew />
       <TodoList />
     </div>
   );
--- src/features/TodoNew.tsx
+++ src/features/TodoNew.tsx
@@ -1,1 +1,25 @@
 import React, { useState } from "react";
+import { useStore } from "../helpers/use-store";
+import { onEnterPress } from "../helpers/use-enter";
+
+export const TodoNew = () => {
+  const [newTodo, setTodo] = useState("");
+  const todoList = useStore();
+
+  const addTodo = () => {
+    todoList.addTodo(newTodo);
+    setTodo("");
+  };
+
+  return (
+    <div className="todo-new">
+      <input
+        type="text"
+        value={newTodo}
+        onKeyDown={onEnterPress(addTodo)}
+        onChange={e => setTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+    </div>
+  );
+};
